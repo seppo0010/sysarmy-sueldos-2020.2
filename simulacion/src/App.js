8615a71554bb8abfbaf391fa5e77c9414aeed644
Sebastian Waisbrot <seppo0010@gmail.com>
2020-09-06 09:08:36 -0300

    Update dates

--- simulacion/src/App.js
+++ simulacion/src/App.js
@@ -197,7 +197,7 @@ class App extends Component {
         <p>Complet&aacute; el formulario siguiente y obten&eacute; una estimaci&oacute;n del sueldo bruto que podr&iacute;as estar ganando.</p>
         <p>El sueldo se estima de acuerdo a un modelo armado de datos recolectados en la encuesta an&oacute;nima.</p>
         <p>Si te interesa saber c&oacute;mo est&aacute;n armados, pod&eacute;s leer el paso a paso <a href="https://seppo0010.github.io/sysarmy-sueldos-2020.1/text/prediccion-de-sueldo/index.html">aqu&iacute;</a>.</p>
-        <p>Los modelos se armaron con datos recolectados en la <a href="https://sysarmy.com/blog/posts/resultados-de-la-encuesta-de-sueldos-2020-2/" target="_blank" rel="noopener noreferrer">encuesta de sysarmy</a> llevada entre diciembre de 2019 y febrero de 2020.</p>
+        <p>Los modelos se armaron con datos recolectados en la <a href="https://sysarmy.com/blog/posts/resultados-de-la-encuesta-de-sueldos-2020-2/" target="_blank" rel="noopener noreferrer">encuesta de sysarmy</a> llevada entre julio de 2020 y agosto de 2020.</p>
         <p>Los resultados son a fines recreativos y no deben usarse para decisiones de contratación. El modelo discrimina por género, edad y orientación sexual, por lo que hacerlo es probablemente ilegal. La muestra usada no es representativa, por lo que los resultados no pueden generalizarse a la población en general.</p>
         <p>La información se procesa en el browser, así que ningún servidor almacena las evaluaciones que se hagan.</p>
         <hr/>
