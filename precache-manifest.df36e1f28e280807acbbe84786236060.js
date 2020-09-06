aa5274a52738967c65f0b37d50308e2598756dc3
Sebastian Waisbrot <seppo0010@gmail.com>
2020-09-06 08:57:22 -0300

    Updates

---
+++ precache-manifest.df36e1f28e280807acbbe84786236060.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7f391534d2762117e265df6dd6ca7cf",
+    "url": "/sysarmy-sueldos-2020.2/index.html"
+  },
+  {
+    "revision": "c5bb0e95e544c47e5868",
+    "url": "/sysarmy-sueldos-2020.2/static/css/main.095e5f73.chunk.css"
+  },
+  {
+    "revision": "d206fa78d70a3e90eded",
+    "url": "/sysarmy-sueldos-2020.2/static/js/2.5659f8b8.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/sysarmy-sueldos-2020.2/static/js/2.5659f8b8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c5bb0e95e544c47e5868",
+    "url": "/sysarmy-sueldos-2020.2/static/js/main.82d1acd7.chunk.js"
+  },
+  {
+    "revision": "00bfedd272ebca582db7",
+    "url": "/sysarmy-sueldos-2020.2/static/js/runtime-main.32ef29a2.js"
+  }
+]);
